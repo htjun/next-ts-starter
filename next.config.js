285fda0eca6f8fd741c165064d3c47bda8c3fa1b
Jason Jun <jason.jun@envato.com>
2022-05-17 13:33:39 +1000

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compilerOptions: {
+    baseUrl: './',
+    paths: {
+      '@components/*': ['components/*'],
+    },
+  },
 }
 
 module.exports = nextConfig
